1146a83e0ab48fea1d92633fc6564f6da5adf08e
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2024-01-02 07:51:58 +0700

    feat(coding with mix): initialize rps

---
+++ lib/rps.ex
@@ -0,0 +1,18 @@
+defmodule Rps do
+  @moduledoc """
+  Documentation for `Rps`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rps.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
